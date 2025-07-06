f25a92ba9931d0af1a8874c41bf34c3a70cba77b
chukwutem-emi <chukwutememi@gmail.com>
2025-07-06 15:47:55 +0100

    added another config files

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    screens: {
+      xs: '375px', // for most phones
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+  },
+  plugins: [],
+}
+
